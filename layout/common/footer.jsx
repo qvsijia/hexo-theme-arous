a00e115baa2daba3e493d02dc92e4dbe1d836495
曲思家 <qvsijia@users.noreply.github.com>
2022-07-21 20:02:38 +0800

    更新

--- layout/common/footer.jsx
+++ layout/common/footer.jsx
@@ -35,7 +35,8 @@ class Footer extends Component {
                         </a>                       
                         <p class="is-size-7">
                             <span dangerouslySetInnerHTML={{ __html: `&copy; ${siteYear} ${author || siteTitle}` }}></span>
-                            &nbsp;&nbsp;Powered by <a href="https://www.qvsijia.com/" target="_blank" rel="noopener">Qvsijia</a>
+                            &nbsp;&nbsp;Powered by <a href="https://hexo.io/" target="_blank" rel="noopener">Hexo</a>&nbsp;&&nbsp;
+                            <a href="https://github.com/qvsijia/hexo-theme-arous" target="_blank" rel="noopener">AROUS</a>
                             {showVisitorCounter ? <br /> : null}
                             {showVisitorCounter ? <span id="busuanzi_container_site_uv"
                                 dangerouslySetInnerHTML={{ __html: visitorCounterTitle }}></span> : null}
